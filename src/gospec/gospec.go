1c383f5f6f248879b3ec1b8f5e86aa15e55040c4
Esko Luontola <esko.luontola@gmail.com>
2009-11-27 00:35:07 +0200

    Use unicode characters in headers, to make sure that all files are correctly saved as UTF-8

--- src/gospec/gospec.go
+++ src/gospec/gospec.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2009 Esko Luontola <www.orfjackal.net>
+// Copyright © 2009 Esko Luontola <www.orfjackal.net>
 // This software is released under the Apache License 2.0.
 // The license text is at http://www.apache.org/licenses/LICENSE-2.0
 
