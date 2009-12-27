546ced9e9e9635eac98f29524ae4bcb52c9118a0
Esko Luontola <esko.luontola@gmail.com>
2009-12-27 18:55:35 +0200

    Getting the current location in source code

---
+++ src/gospec/location.go
@@ -0,0 +1,48 @@
+// Copyright © 2009 Esko Luontola <www.orfjackal.net>
+// This software is released under the Apache License 2.0.
+// The license text is at http://www.apache.org/licenses/LICENSE-2.0
+
+package gospec
+
+import (
+	"fmt"
+	"runtime"
+	"strings"
+)
+
+
+type Location struct {
+	file string
+	line int
+}
+
+func currentLocation() *Location {
+	return newLocation(1)
+}
+
+func callerLocation() *Location {
+	return newLocation(2)
+}
+
+func newLocation(n int) *Location {
+	if _, file, line, ok := runtime.Caller(n + 1); ok {
+		return &Location{filename(file), line}
+	}
+	return nil
+}
+
+func filename(path string) string {
+	names := strings.Split(path, "/", 0)
+	if len(names) > 0 {
+		return names[len(names) - 1]
+	}
+	return path
+}
+
+func (loc *Location) String() string {
+	if loc == nil {
+		return "Unknown File"
+	}
+	return fmt.Sprintf("%v:%v", loc.file, loc.line)
+}
+
---
+++ src/gospec/location_test.go
@@ -0,0 +1,39 @@
+// Copyright © 2009 Esko Luontola <www.orfjackal.net>
+// This software is released under the Apache License 2.0.
+// The license text is at http://www.apache.org/licenses/LICENSE-2.0
+
+package gospec
+
+import (
+	"strings"
+	"testing"
+)
+
+
+func Test__Get_the_location_of_current_method(t *testing.T) {
+	loc := currentLocation()
+	assertHasPrefix(loc.String(), "location_test.go:", t)
+}
+
+func Test__Get_the_location_of_calling_method(t *testing.T) {
+	loc := callerLocation()
+	assertHasPrefix(loc.String(), "testing.go:", t)
+}
+
+func Test__Failing_to_get_the_location_fails_grafecully_with_an_error_message(t *testing.T) {
+	loc := newLocation(1000)
+	assertEquals("Unknown File", loc.String(), t)
+}
+
+func Test__Calls_to_newLocation_are_synced_with_the_helper_methods(t *testing.T) {
+	assertEquals(currentLocation().String(), newLocation(0).String(), t)
+	assertEquals(callerLocation().String(), newLocation(1).String(), t)
+}
+
+
+func assertHasPrefix(s string, prefix string, t *testing.T) {
+	if !strings.HasPrefix(s, prefix) {
+		t.Error("Expected prefix", prefix, "but it was not:", s)
+	}
+}
+
