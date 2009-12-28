2d836336e2bd734d1bdcf37358bae86e2e42f576
Esko Luontola <esko.luontola@gmail.com>
2009-12-29 00:26:02 +0200

    Examples on using asserts

---
+++ examples/all_specs_test.go
@@ -0,0 +1,23 @@
+// Copyright © 2009 Esko Luontola <www.orfjackal.net>
+// This software is released under the Apache License 2.0.
+// The license text is at http://www.apache.org/licenses/LICENSE-2.0
+
+package examples
+
+import (
+	"gospec"
+	"testing"
+)
+
+
+// You will need to list every spec in a TestXxx method like this,
+// so that gotest can be used to run the specs. Later GoSpec might
+// get its own command line tool similar to gotest, but for now this
+// is the way to go. This shouldn't require too much typing, because
+// there will be typically only one top-level spec per class/feature.
+func TestAllSpecs(t *testing.T) {
+	r := gospec.NewRunner()
+	r.AddSpec("AssertExamplesSpec", AssertExamplesSpec)
+	gospec.MainGoTest(r, t)
+}
+
---
+++ examples/dummies.go
@@ -0,0 +1,50 @@
+// Copyright © 2009 Esko Luontola <www.orfjackal.net>
+// This software is released under the Apache License 2.0.
+// The license text is at http://www.apache.org/licenses/LICENSE-2.0
+
+package examples
+
+
+// Point2 implements the gospec.Equality interface, so it can be
+// compared for equality in GoSpec specs.
+//
+// If the type can be used as a value, like in this case, then extra
+// care is needed when writing the Equals method because the other
+// object can be a value or a pointer to a value.
+type Point2 struct {
+	X, Y int
+}
+
+func (this Point2) Equals(other interface{}) bool {
+	switch that := other.(type) {
+	case Point2:
+		return this.equals(&that)
+	case *Point2:
+		return this.equals(that)
+	}
+	return false
+}
+
+func (this *Point2) equals(that *Point2) bool {
+	return this.X == that.X &&
+	       this.Y == that.Y
+}
+
+
+// Point3 also implements the gospec.Equality interface, but unlike
+// with Point2, here we assume that Point3 will not be used as a value.
+// That makes the Equals method somewhat simpler.
+type Point3 struct {
+	X, Y, Z int
+}
+
+func (this *Point3) Equals(other interface{}) bool {
+	switch that := other.(type) {
+	case *Point3:
+		return this.X == that.X &&
+		       this.Y == that.Y &&
+		       this.Z == that.Z
+	}
+	return false
+}
+
