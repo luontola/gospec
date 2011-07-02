5ddd533de7d0f9964749e324e3eb97bc383010c8
Esko Luontola <esko.luontola@gmail.com>
2011-07-02 14:31:02 +0300

    Refactored

--- release.rb
+++ release.rb
@@ -4,6 +4,13 @@ def update_urls(s, tag)
 	s.gsub(/\/(tree|blob)\/master\//, "/\\1/#{tag}/")
 end
 
+def sys(*command)
+	def add_quotes(ss)
+		ss.map { |s| (s.include? ' ') ? '"'+s+'"' : s }.join(' ')
+	end
+	system(*command) or raise "command failed: #{add_quotes(command)}"
+end
+
 version = ARGV[0]
 raise "version number must be in format 'x.y.z' but was '#{version}'" unless version =~ /\A\d+\.\d+\.\d+\z/m
 README_FILE = 'README.md'
@@ -27,44 +34,43 @@ next_readme = readme_parts[0] + next_title + release_title + readme_parts[2]
 # commit and tag release
 raise "must be in master branch" unless `git branch` =~ /^\* master$/
 File.open(README_FILE, 'w') { |f| f.write(release_readme) }
-system('git', 'add', README_FILE) or raise
-system('git', 'commit', '-m', "Release #{version}") or raise
-system('git', 'tag', tag) or raise
+sys('git', 'add', README_FILE)
+sys('git', 'commit', '-m', "Release #{version}")
+sys('git', 'tag', tag)
 
 # update release branch
-system('git', 'checkout', 'release') or raise
-system('git', 'merge', '--ff-only', tag) or raise
-system('git', 'checkout', 'master') or raise
+sys('git', 'checkout', 'release')
+sys('git', 'merge', '--ff-only', tag)
+sys('git', 'checkout', 'master')
 
 # commit next iteration
 File.open(README_FILE, 'w') { |f| f.write(next_readme) }
-system('git', 'add', README_FILE) or raise
-system('git', 'commit', '-m', "Prepare for next development iteration") or raise
+sys('git', 'add', README_FILE)
+sys('git', 'commit', '-m', "Prepare for next development iteration")
 
 # manual check before pushing the release
-system('gitk', '--all') or raise
+sys('gitk', '--all')
 puts "\nProceed with release? (yes/no)"
 if STDIN.gets.chomp == 'yes'
 
 	# push
-	system('git', 'push', 'origin', 'master:master') or raise
-	system('git', 'push', 'origin', 'release:release') or raise
-	system('git', 'push', '--tags', 'origin') or raise
+	sys('git', 'push', 'origin', 'master:master')
+	sys('git', 'push', 'origin', 'release:release')
+	sys('git', 'push', '--tags', 'origin')
 
 	puts "\nRelease done."
 else
 
 	# undo tag
-	system('git', 'tag', '-d', tag) or raise
+	sys('git', 'tag', '-d', tag)
 	
 	# undo release branch
-	system('git', 'checkout', 'release') or raise
-	system('git', 'reset', '--hard', 'origin/release') or raise
+	sys('git', 'checkout', 'release')
+	sys('git', 'reset', '--hard', 'origin/release')
 	
 	# undo master branch
-	system('git', 'checkout', 'master') or raise
-	system('git', 'reset', '--hard', 'HEAD~2') or raise
+	sys('git', 'checkout', 'master')
+	sys('git', 'reset', '--hard', 'HEAD~2')
 
 	puts "\nRelease aborted."
 end
-
